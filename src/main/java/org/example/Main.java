0d222ee4db0dc90c7ab8c81aa25e623809a420ae
Nishtha Paul <33231250+nishthapaul@users.noreply.github.com>
2024-02-18 05:37:35 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
+        System.out.println("Hello and welcome!");
         System.out.println(new Book().title(1, 2));
     }
 }
